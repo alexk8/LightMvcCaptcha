3805ddd4975ed885c037960881f45511b4f502a5
cygwin255 <cygwin255@gmail.com>
2017-06-04 18:55:47 +0300

    Add abstract controller for providing captcha http GET action

---
+++ LightMvcCaptcha/LightMvcCaptcha.Core/CaptchaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LightMvcCaptcha.Core
+{
+    public abstract class CaptchaController : Controller
+    {
+        public FileStreamResult GetCaptcha()
+        {
+            using (var captcha = Captcha.Generate())
+            {
+                Session["CAPTCHA"] = captcha;
+
+                return new FileStreamResult(captcha.ImageStream, "image/jpeg");
+            }
+        }
+    }
+}
