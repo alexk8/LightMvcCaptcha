59f5e3baee555c6bc9e54b1a9ffd54222d1e9ec3
cygwin255 <cygwin255@gmail.com>
2017-06-04 18:09:10 +0300

    Set up basic Web controller for testing purposes

---
+++ LightMvcCaptcha/LightMvcCaptcha.Core/CaptchaAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LightMvcCaptcha.Core
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CaptchaAttribute : ValidationAttribute
+    {
+        public CaptchaAttribute()
+        {
+            
+        }
+
+        public override bool IsValid(object value)
+        {
+            string key = value as string;
+            Captcha captcha = HttpContext.Current.Session["CAPTCHA"] as Captcha;
+            
+            return key != null && captcha != null && captcha.Key == key.ToUpper();
+        }
+    }
+}
---
+++ LightMvcCaptcha/LightMvcCaptcha.Web/ViewModels/CaptchaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LightMvcCaptcha.Core;
+
+namespace LightMvcCaptcha.Web.ViewModels
+{
+    public class CaptchaViewModel
+    {
+        [Captcha(ErrorMessage = "Wrong captcha")]
+        public string Captcha { get; set; }
+    }
+}
